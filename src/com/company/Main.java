fc6f01b823c79136b31a352018aceff85dbb3659
Ilya <awd@gmail.com>
2019-11-08 13:31:06 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("*Don't*");
+		System.out.println("*worry*");
+		System.out.println("*be*");
+		System.out.println("*happy*");
+	}
+}
+
+
+
